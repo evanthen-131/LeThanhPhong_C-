24c28db910837291655ec6c7f0cd3b8ce37474f4
LeThanhPhong_C- <ltp2003z@gmai.com>
2023-10-15 15:21:35 +0700

    add  buoi 4

---
+++ Buoi4_2/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace Buoi4_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbKhoa = new System.Windows.Forms.ComboBox();
+            this.txtDiemTB = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.mtxtMaSV = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dgvDanhSach = new System.Windows.Forms.DataGridView();
+            this.masv = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hoten = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.diemtb = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.khoa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDanhSach)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(355, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "QUẢN LÝ SINH VIÊN";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbKhoa);
+            this.groupBox1.Controls.Add(this.txtDiemTB);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtHoTen);
+            this.groupBox1.Controls.Add(this.mtxtMaSV);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(51, 65);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(853, 140);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin";
+            // 
+            // cbKhoa
+            // 
+            this.cbKhoa.FormattingEnabled = true;
+            this.cbKhoa.Location = new System.Drawing.Point(578, 85);
+            this.cbKhoa.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbKhoa.Name = "cbKhoa";
+            this.cbKhoa.Size = new System.Drawing.Size(203, 21);
+            this.cbKhoa.TabIndex = 5;
+            // 
+            // txtDiemTB
+            // 
+            this.txtDiemTB.Location = new System.Drawing.Point(578, 34);
+            this.txtDiemTB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtDiemTB.Name = "txtDiemTB";
+            this.txtDiemTB.Size = new System.Drawing.Size(203, 20);
+            this.txtDiemTB.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(475, 91);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Khoa";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(475, 38);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Điểm TB";
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(131, 86);
+            this.txtHoTen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(212, 20);
+            this.txtHoTen.TabIndex = 2;
+            // 
+            // mtxtMaSV
+            // 
+            this.mtxtMaSV.Location = new System.Drawing.Point(131, 34);
+            this.mtxtMaSV.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.mtxtMaSV.Name = "mtxtMaSV";
+            this.mtxtMaSV.Size = new System.Drawing.Size(212, 20);
+            this.mtxtMaSV.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 91);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Họ tên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 34);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mã SV";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnThoat);
+            this.groupBox2.Controls.Add(this.btnXoa);
+            this.groupBox2.Controls.Add(this.btnSua);
+            this.groupBox2.Controls.Add(this.btnThem);
+            this.groupBox2.Location = new System.Drawing.Point(51, 243);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Size = new System.Drawing.Size(853, 65);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Chức năng";
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(657, 18);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(166, 33);
+            this.btnThoat.TabIndex = 3;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click_1);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(445, 18);
+            this.btnXoa.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(162, 36);
+            this.btnXoa.TabIndex = 2;
+            this.btnXoa.Text = "Xoá";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(231, 18);
+            this.btnSua.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(158, 33);
+            this.btnSua.TabIndex = 1;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(15, 17);
+            this.btnThem.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(146, 33);
+            this.btnThem.TabIndex = 0;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dgvDanhSach);
+            this.groupBox3.Location = new System.Drawing.Point(51, 320);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox3.Size = new System.Drawing.Size(761, 156);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Danh sách";
+            // 
+            // dgvDanhSach
+            // 
+            this.dgvDanhSach.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDanhSach.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.masv,
+            this.hoten,
+            this.diemtb,
+            this.khoa});
+            this.dgvDanhSach.Location = new System.Drawing.Point(15, 24);
+            this.dgvDanhSach.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvDanhSach.Name = "dgvDanhSach";
+            this.dgvDanhSach.RowHeadersWidth = 62;
+            this.dgvDanhSach.RowTemplate.Height = 28;
+            this.dgvDanhSach.Size = new System.Drawing.Size(733, 105);
+            this.dgvDanhSach.TabIndex = 0;
+            this.dgvDanhSach.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDanhSach_CellClick);
+            // 
+            // masv
+            // 
+            this.masv.HeaderText = "Mã sinh viên";
+            this.masv.MinimumWidth = 8;
+            this.masv.Name = "masv";
+            this.masv.Width = 150;
+            // 
+            // hoten
+            // 
+            this.hoten.HeaderText = "Họ tên";
+            this.hoten.MinimumWidth = 8;
+            this.hoten.Name = "hoten";
+            this.hoten.Width = 150;
+            // 
+            // diemtb
+            // 
+            this.diemtb.HeaderText = "Điểm TB";
+            this.diemtb.MinimumWidth = 8;
+            this.diemtb.Name = "diemtb";
+            this.diemtb.Width = 150;
+            // 
+            // khoa
+            // 
+            this.khoa.HeaderText = "Khoa";
+            this.khoa.MinimumWidth = 8;
+            this.khoa.Name = "khoa";
+            this.khoa.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 487);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDanhSach)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbKhoa;
+        private System.Windows.Forms.TextBox txtDiemTB;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.MaskedTextBox mtxtMaSV;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView dgvDanhSach;
+        private System.Windows.Forms.DataGridViewTextBoxColumn masv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hoten;
+        private System.Windows.Forms.DataGridViewTextBoxColumn diemtb;
+        private System.Windows.Forms.DataGridViewTextBoxColumn khoa;
+    }
+}
+
---
+++ Buoi4_2/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Buoi4_2
+{
+    public partial class Form1 : Form
+    {
+        int rowindex = -1;
+        String[] listKhoa = { "Công nghệ thông tin", "Kế toán", "Ngoại ngữ", "Điện tử" };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbKhoa.DataSource = listKhoa;
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            double diemtb;
+            try
+            {
+                if (!mtxtMaSV.Text.Length.Equals(10))
+                {
+                    throw new Exception("Mssv quá 10 ký tự");
+                }
+                if (this.checkMaSV(mtxtMaSV.Text) == false)
+                {
+                    throw new Exception("Mssv Đã tồn tại");
+                }
+                if (txtHoTen.Text.Length.Equals(0))
+                {
+                    throw new Exception("Không được để trống Họ Tên");
+                }
+                if (!double.TryParse(txtDiemTB.Text, out diemtb))
+                {
+                    throw new Exception("Điểm TB không hợp lệ");
+                }
+                string masv = mtxtMaSV.Text;
+                string hoten = txtHoTen.Text;
+                string khoa = cbKhoa.Text;
+                int row = dgvDanhSach.Rows.Add();
+                dgvDanhSach.Rows[row].Cells["MaSV"].Value = masv;
+                dgvDanhSach.Rows[row].Cells["HoTen"].Value = hoten;
+                dgvDanhSach.Rows[row].Cells["DiemTB"].Value = diemtb;
+                dgvDanhSach.Rows[row].Cells["Khoa"].Value = khoa;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Thông bao");
+            }
+        }
+        public bool checkMaSV(string masv)
+        {
+            if (dgvDanhSach.Rows.Count == 0)
+            {
+                return true;
+            }
+            for (int row = 0; row < dgvDanhSach.Rows.Count - 1; row++)
+            {
+                if (dgvDanhSach.Rows[row].Cells["MaSV"].Value.ToString() == masv)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void dgvDanhSach_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            rowindex = e.RowIndex;
+            if (rowindex != -1 && rowindex < dgvDanhSach.Rows.Count - 1)
+            {
+                mtxtMaSV.Text = dgvDanhSach.Rows[rowindex].Cells["MaSV"].Value.ToString();
+                txtHoTen.Text = dgvDanhSach.Rows[rowindex].Cells["HoTen"].Value.ToString();
+                txtDiemTB.Text = dgvDanhSach.Rows[rowindex].Cells["DiemTB"].Value.ToString();
+                string tenkhoa = dgvDanhSach.Rows[rowindex].Cells["Khoa"].Value.ToString();
+                int i = 0;
+                while (i < listKhoa.Length && listKhoa[i] != tenkhoa)
+                {
+                    i++;
+                }
+                {
+                    cbKhoa.SelectedIndex = i;
+                }
+
+            }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            double diemtb;
+            try
+            {
+
+                if (rowindex == -1 || rowindex >= dgvDanhSach.Rows.Count - 1)
+                {
+                    throw new Exception("Chọn Sinh Viên Cần Sửa!");
+                }
+                if (!mtxtMaSV.Text.Length.Equals(10))
+                {
+                    throw new Exception("Ma sinh vien 10 ký tự");
+                }
+                if (this.checkMaSV(mtxtMaSV.Text) == false && mtxtMaSV.Text != dgvDanhSach.Rows[rowindex].Cells["MaSV"].Value.ToString())
+                    throw new Exception("Ma sinh vien da ton tại");
+                if (txtHoTen.Text.Length.Equals(0))
+                {
+                    throw new Exception("Ho ten khong dược đe trong");
+                }
+                if (!double.TryParse(txtDiemTB.Text, out diemtb))
+                {
+                    throw new Exception("Điem TB khong phải so");
+                }
+                string masv = mtxtMaSV.Text;
+                string hoten = txtHoTen.Text;
+                string khoa = cbKhoa.Text;
+                dgvDanhSach.Rows[rowindex].Cells["MaSV"].Value = masv;
+                dgvDanhSach.Rows[rowindex].Cells["HoTen"].Value = hoten;
+                dgvDanhSach.Rows[rowindex].Cells["DiemTB"].Value = diemtb;
+                dgvDanhSach.Rows[rowindex].Cells["Khoa"].Value = khoa;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (rowindex == -1 || rowindex >= dgvDanhSach.Rows.Count - 1)
+                {
+                    throw new Exception("Chưa chọn sinh viên cần xoá!");
+                }
+                dgvDanhSach.Rows.RemoveAt(rowindex);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Thông báo");
+            }
+        }
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            {
+                DialogResult result = MessageBox.Show(
+                    "Bạn có muốn thoát không",
+                    "Thông báo",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question
+                    );
+                if (result == DialogResult.Yes)
+                {
+                    Application.Exit();
+                }
+
+            }
+        }
+
+        private void btnThoat_Click_1(object sender, EventArgs e)
+        {
+            DialogResult dialog;
+            dialog = MessageBox.Show("Bạn có muốn thoát hay không", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dialog == DialogResult.Yes)
+                Application.Exit();
+        }
+    }
+}
